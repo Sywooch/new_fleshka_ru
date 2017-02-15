5c20489c7b428624bdb5aaca35dabf3a6db94444
Dilshod89 <br.dilshod@gmail.com>
2017-02-15 16:43:35 +0500

    ert

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -195,12 +195,12 @@ $curRegion = \Yii::$app->session['region'];
                             <div class="custom-block">
                                 <i class="icon-phone" style="margin-right: 5px;">
                                     <span>                                        
-                                        <a style="font-size: 17px;" href="tel:79647974238">+7 (964) 797-42-38</a>
+                                        <a style="font-size: 17px;" href="tel:79647974238"><b>+7 (964) 797-42-38</b></a>
                                     </span>
                                 </i>
                                 <i class="icon-phone" style="margin-right: 5px;">
                                     <span>
-                                        <a style="font-size: 17px;" href="tel:74957808093">+7 (495) 780-80-93</a>
+                                        <a style="font-size: 17px;" href="tel:74957808093"><b>+7 (495) 780-80-93</b></a>
                                     </span>
                                 </i>
                                 <span class="split"></span>
