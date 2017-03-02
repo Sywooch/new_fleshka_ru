a60124b9546dc6c47eec1c1e683153e2de36ffa5
Dilshod <br.dilshod@gmail.com>
2017-03-02 11:54:33 +0500

    345

--- frontend/views/page/view.php
+++ frontend/views/page/view.php
@@ -12,28 +12,33 @@
                     <?php if ($model['url'] == 'kontakty'): ?>
                         <div class="row">                            
                             <div class="col-lg-5">
-                                <h2>Обратная связь</h2>
-                                <?php $form = \yii\widgets\ActiveForm::begin(['id' => 'contact-form']); ?>
+                                <?php if (Yii::$app->session->getFlash('success')): ?>
+                                    <h1>Спасибо!</h1>
+                                    <p><?= Yii::$app->session->getFlash('success'); ?></p>
+                                <?php else: ?>
+                                    <h2>Обратная связь</h2>
+                                    <?php $form = \yii\widgets\ActiveForm::begin(['id' => 'contact-form']); ?>
 
-                                <?= $form->field($feedBack, 'name')->textInput(['autofocus' => true]) ?>
+                                    <?= $form->field($feedBack, 'name')->textInput(['autofocus' => true]) ?>
 
-                                <?= $form->field($feedBack, 'email') ?>
+                                    <?= $form->field($feedBack, 'email') ?>
 
-                                <?= $form->field($feedBack, 'subject') ?>
+                                    <?= $form->field($feedBack, 'subject') ?>
 
-                                <?= $form->field($feedBack, 'body')->textArea(['rows' => 6]) ?>
+                                    <?= $form->field($feedBack, 'body')->textArea(['rows' => 6]) ?>
 
-                                <?=
-                                $form->field($feedBack, 'verifyCode')->widget(yii\captcha\Captcha::className(), [
-                                    'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
-                                ])
-                                ?>
+                                    <?=
+                                    $form->field($feedBack, 'verifyCode')->widget(yii\captcha\Captcha::className(), [
+                                        'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
+                                    ])
+                                    ?>
 
-                                <div class="form-group">
-                                    <?= yii\bootstrap\Html::submitButton('Отправить сообщение', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
-                                </div>
+                                    <div class="form-group">
+                                        <?= yii\bootstrap\Html::submitButton('Отправить сообщение', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
+                                    </div>
 
-                                <?php \yii\widgets\ActiveForm::end(); ?>
+                                    <?php \yii\widgets\ActiveForm::end(); ?>
+                                <?php endif; ?>
                             </div>
                         </div>
                     <?php endif; ?>
