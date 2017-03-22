ad31f9d7b9c7d92a8c1c758e2877ed77e5a3f517
Dilshod <br.dilshod@gmail.com>
2017-03-22 13:27:55 +0500

    цук

--- frontend/widgets/sliders/views/mainSlider.php
+++ frontend/widgets/sliders/views/mainSlider.php
@@ -4,6 +4,11 @@ $assets = Yii::getAlias('@web');
 <div id="banner-slider-demo-5" class="owl-carousel owl-theme owl-middle-narrow owl-banner-carousel">
     <div class="item">
         <div class="container" style="position:relative">
+            <a href="/category/zaryadnye-ustroistva---portativnye-akkumulyatory-powerbank-optom-84"><img src="<?= $assets ?>/images/banners/new_banner.jpg" alt="" /></a>
+        </div>
+    </div>
+    <div class="item">
+        <div class="container" style="position:relative">
             <a href="/catalog"><img src="<?= $assets ?>/images/banners/bb1.jpg" alt="" /></a>
         </div>
     </div>
@@ -17,11 +22,7 @@ $assets = Yii::getAlias('@web');
             <a href="/catalog"><img src="<?= $assets ?>/images/banners/bb3.jpg" alt="" /></a>
         </div>
     </div>
-    <div class="item">
-        <div class="container" style="position:relative">
-            <a href="/category/zaryadnye-ustroistva---portativnye-akkumulyatory-powerbank-optom-84"><img src="<?= $assets ?>/images/banners/new_banner.jpg" alt="" /></a>
-        </div>
-    </div>
+    
 </div>
 <script type="text/javascript">
     jQuery(function ($) {
