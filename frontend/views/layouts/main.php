4e3d9ad3f0c32e701962028918bf7aa2b5b85103
Dilshod <br.dilshod@gmail.com>
2017-04-12 16:39:55 +0500

    wer

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -861,11 +861,7 @@ $curRegion = \Yii::$app->session['region'];
                 } catch (e) {
                 }
             })(document);/* ]]> */</script>
-<!-- BEGIN JIVOSITE CODE {literal} -->
-<script type='text/javascript'>
-(function(){ var widget_id = 'b7sbJDOnLn';var d=document;var w=window;function l(){
-var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();</script>
-<!-- {/literal} END JIVOSITE CODE -->
+
     </body>
 </html>
 <?php $this->registerJsFile($assets . '/js/jquery.inputmask.bundle.js'); ?>
